981007606b1da10936cf7d7d03cac28cbf660ddd
Emerson Costa <costtasemerson@gmail.com>
2019-05-02 20:50:54 -0300

    [ADD] Adicionando exemplo para o cut/put box

--- Escultor/main.cpp
+++ Escultor/main.cpp
@@ -25,25 +25,31 @@ int main()
 
 
     /*
-        ***************************************************************
-        ***** Nesse exemplo é possível ver o Cut/Put Box,         *****
-        ***** além do setColor para, nesse caso com vermelho = 1  *****
-        ***** e alpha = 0.7 .Nesse exemplo também é utilizado os  *****
-        ***** métodos cutVoxel e putVoxel                         *****
-        **************************************************************
+        ******************************************************************
+        ***** Nesse exemplo é possível ver o Cut/Put Box, além do    *****
+        ***** setColor para, nesse caso com azul = 1  e alpha = 0.3  *****
+        ***** Nesse exemplo também é utilizado os métodos cutVoxel e *****
+        ***** putVoxel                                              *****
+        ******************************************************************
+
+        s.setColor(0, 0, 1, 0.3);
+        s.putBox(40,95,40,95,40,95);
+        s.cutBox(80,95,80,95,80,95);
     */
 
     /*
-    s.putBox(50,95,50,95,50,95);
-    s.cutBox(90,95,90,95,90,95);
-    s.setColor(1, 0, 0, 0.5);
-    s.putSphere(10,10,10,9);
-    s.putEllipsoid(50,50,50,49,49,49);
-    s.cutSphere(25,25,25,24);
-    s.cutEllipsoid(40,40,40,39,39,39);
+        ***************************************************************
+        ***** Nesse exemplo é possível ver o Cut/Put Box,         *****
+        ***** além do setColor para, nesse caso com azul = 1      *****
+        ***** e alpha = 0.7 .Nesse exemplo também é utilizado os  *****
+        ***** métodos cutVoxel e putVoxel                         *****
+        ***************************************************************
+
+        s.putEllipsoid(50,50,50,49,49,49);
+        s.cutEllipsoid(40,40,40,39,39,39);
     */
 
-    s.writeVECT("/home/emerson/workspace/arquivo");
+    s.writeOFF("/home/emerson/workspace/arquivo");
 
     cout << "O arquivo foi gerado" << endl;
 
