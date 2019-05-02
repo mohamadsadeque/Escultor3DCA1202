fae4c009591d0bb1cb83459bd979f26c2a5f6a9e
Emerson Costa <costtasemerson@gmail.com>
2019-05-02 08:45:41 -0300

    fix

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+#include <sculptor3d.h>
+
+using namespace std;
+
+int main()
+{
+    sculptor3d scp(10, 10, 10);
+
+    scp.setColor(1, 0, 0, 1);
+    scp.putBox(1, 9, 1, 9, 1, 9);
+    scp.writeVECT("/home/emerson/workspace/arquivo");
+
+    cout << "O arquivo foi gerado" << endl;
+
+}
